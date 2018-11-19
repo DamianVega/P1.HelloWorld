8c729b874326637b9069fbc6f0c3c35df579b2a4
DamianVega <44816484+DamianVega@users.noreply.github.com>
2018-11-19 12:03:55 -0700

    Add files via upload

---
+++ MathOperationsP5.py
@@ -0,0 +1,18 @@
+a = 25
+b = 10
+c = 7
+# Adding
+print("Display the sum of:",a,"+",c,"=",a+c)
+# Subtracting
+print("Display the difference of:",a,"-",c,"=",a-c)
+# Multiplying
+print("Display the multiplication of:",a,"*",c,"=",a*c)
+# Division
+print("Display the division of:",a,"/",c,"=",a/c)
+# Integer Division
+print("Display the integer division of:",a,"//",c,"=",a//c)
+# The remainder of Integer Division, %
+print("Display the remainder of integer division of:",a,"%",c,"=",a%c)
+print(a, "Modulus",c,"=",a%c)
+# Power of a Number
+print("2 to the 5th power =",2**5)
