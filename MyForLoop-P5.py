b2cc503e94839a96a06a2a41d0b74d25135ba9f7
DamianVega <44816484+DamianVega@users.noreply.github.com>
2019-01-08 12:03:10 -0700

    Add files via upload

---
+++ MyForLoop-P5.py
@@ -0,0 +1,15 @@
+# print()
+# for i in range(1,10):
+#     print(i)
+# for j in range(1,10):
+#     print(j,end='')
+# print()
+# for k in range(1,10):
+#     print(k,' ',end='')
+# print()
+# for s in range(1,1000,100):
+#     print('{0:5d}'.format(s))
+for row in range(1,10):
+    for col in range(1,5):
+        print(row,col,' ',end='')
+    print()
