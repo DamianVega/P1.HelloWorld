11065c1169133ab58d4338b606c583dcaf231e46
DamianVega <44816484+DamianVega@users.noreply.github.com>
2019-03-22 12:23:43 -0700

    Add files via upload

--- MyGameP5.py
+++ MyGameP5.py
@@ -79,7 +79,7 @@ class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((10, 10))
-        self.image.fill(YELLOW)
+        self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
